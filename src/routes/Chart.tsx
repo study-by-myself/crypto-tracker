035249113a96ae02841d96f805809d8e4c771ce1
Luna <luvbliny@gmail.com>
2022-04-13 16:50:06 +0900

    Feat: prettier chart but not pretty code

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -18,32 +18,44 @@ interface IHistorical {
   market_cap: number;
 }
 
-const options: ApexOptions = {
-  theme: {
-    mode: "dark",
-  },
-  chart: {
-    height: 300,
-    width: 500,
-    toolbar: {
-      show: false,
-    },
-    background: "transparent",
-  },
-  grid: { show: false },
-  stroke: {
-    curve: "smooth",
-    width: 4,
-  },
-  yaxis: {
-    show: false,
-  },
-  xaxis: {
-    axisBorder: { show: false },
-    axisTicks: { show: false },
-    labels: { show: false },
-  },
-};
+//const options: ApexOptions = (data: IHistorical[]) => ({
+//  theme: {
+//    mode: "dark",
+//  },
+//  chart: {
+//    height: 300,
+//    width: 500,
+//    toolbar: {
+//      show: false,
+//    },
+//    background: "transparent",
+//  },
+//  grid: { show: false },
+//  stroke: {
+//    curve: "smooth",
+//    width: 4,
+//  },
+//  yaxis: {
+//    show: false,
+//  },
+//  xaxis: {
+//    axisBorder: { show: false },
+//    axisTicks: { show: false },
+//    labels: { show: false },
+//    type: "datetime",
+//    categories: data?.map((price: IHistorical) => price.time_close),
+//  },
+//  fill: {
+//    type: "gradient",
+//    gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+//  },
+//  colors: ["#0fbcf9"],
+//  tooltip: {
+//    y: {
+//      formatter: (value: number) => `$${value.toFixed(2)}`,
+//    },
+//  },
+//});
 
 function Chart({ coinId }: ChartProps) {
   const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
@@ -63,7 +75,44 @@ function Chart({ coinId }: ChartProps) {
               data: data?.map((price) => price.close) as number[],
             },
           ]}
-          options={options}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: { show: false },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+              type: "datetime",
+              categories: data?.map((price: IHistorical) => price.time_close),
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+            },
+            colors: ["#0fbcf9"],
+            tooltip: {
+              y: {
+                formatter: (value: number) => `$${value.toFixed(2)}`,
+              },
+            },
+          }}
         />
       )}
     </div>
